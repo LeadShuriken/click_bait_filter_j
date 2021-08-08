68b0556886ea23e2f8efdce19050558a5c3b6a22
LeadShuriken <d.s.mishev@gmail.com>
2021-08-09 01:42:49 +0200

    Typo

--- src/main/java/com/clickbait/plugin/controllers/ClickBaitApiController.java
+++ src/main/java/com/clickbait/plugin/controllers/ClickBaitApiController.java
@@ -3,7 +3,6 @@ package com.clickbait.plugin.controllers;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
